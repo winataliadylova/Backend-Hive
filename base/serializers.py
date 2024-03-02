260a9fd5fa70e522124c7d63233928b0d977f007
chiisanahana <mulyaniangel14@gmail.com>
2024-03-03 00:54:27 +0700

    fix: set FK and password fields to write_only in serializers

--- base/serializers.py
+++ base/serializers.py
@@ -5,6 +5,9 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
 class ProviderSerializer(serializers.ModelSerializer):
     # def create(self, validated_data):
@@ -16,6 +19,9 @@ class ProviderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provider
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
         
 class CarFileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,7 +32,10 @@ class CarSerializer(serializers.ModelSerializer):
     provider = ProviderSerializer(read_only=True)
     queryset = Car.objects.prefetch_related('car_files')
     car_files = CarFileSerializer(queryset, many=True, read_only=True)
-
+    
+    provider_id = serializers.PrimaryKeyRelatedField(
+        queryset=Provider.objects.all(), source='provider', write_only=True)
+    
     class Meta:
         model = Car
         fields = '__all__'
@@ -35,24 +44,34 @@ class AdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Admin
         fields = '__all__'
-
-class PaymentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Payment
-        fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
 class OrderSerializer(serializers.ModelSerializer):
     car = CarSerializer(read_only=True)
     customer = CustomerSerializer(read_only=True)
-    queryset = Order.objects.prefetch_related('payments')
-    payments = PaymentSerializer(queryset, many=True, read_only=True)
+    
+    car_id = serializers.PrimaryKeyRelatedField(
+        queryset=Car.objects.all(), source='car', write_only=True)
+    customer_id = serializers.PrimaryKeyRelatedField(
+        queryset=Customer.objects.all(), source='customer', write_only=True)
 
     class Meta:
         model = Order
         fields = '__all__'
 
+class PaymentSerializer(serializers.ModelSerializer):
+    order = OrderSerializer(read_only=True)
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
 class WishlistSerializer(serializers.ModelSerializer):
     car = CarSerializer(read_only=True)
+    car_id = serializers.PrimaryKeyRelatedField(
+        queryset=Car.objects.all(), source='car', write_only=True)
+    
     class Meta:
         model = Wishlist
         fields = '__all__'
@@ -72,6 +91,11 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     provider = ProviderSerializer(read_only=True)
     queryset = Chat.objects.prefetch_related('chat')
     chats = ChatSerializer(queryset, many=True, read_only=True)
+
+    customer_id = serializers.PrimaryKeyRelatedField(
+        queryset=Customer.objects.all(), source='customer', write_only=True)
+    provider_id = serializers.PrimaryKeyRelatedField(
+        queryset=Provider.objects.all(), source='provider', write_only=True)
 
     class Meta:
         model = ChatRoom
